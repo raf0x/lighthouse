e10d8ddebdbc66e08ac789ccca8fac76ed4f802f
raf0x <rlemor@gmail.com>
2026-05-15 07:45:44 -0500

    Refactor account health evaluation functions
    Removed getPortfolioContext function and added calculateHealthScore function to evaluate account health based on various metrics.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,13 @@
-export function getPortfolioContext(account: any, allAccounts: any[]) {
-  const avgHealth = allAccounts.reduce((sum, acc) => 
-    sum + calculateHealthScore(acc), 0) / allAccounts.length;
-  
-  const accountHealth = calculateHealthScore(account);
-  const percentile = (allAccounts.filter(acc => 
-    calculateHealthScore(acc) < accountHealth).length / allAccounts.length) * 100;
-  
-  return {
-    portfolioAvg: Math.round(avgHealth),
-    accountRank: Math.round(percentile),
-    isOutlier: accountHealth < (avgHealth - 20) || accountHealth > (avgHealth + 20),
-  };
+export function calculateHealthScore(account: any): number {
+  let score = 70;
+  const util = account.seats_active / account.seats_total;
+  if (util > 0.85) score += 10;
+  else if (util < 0.50) score -= 15;
+  const mau = account.monthly_active_users;
+  if (mau[4] > mau[0]) score += 10;
+  else if (mau[4] < mau[0] * 0.9) score -= 20;
+  if (account.support_tickets_30d > 15) score -= 10;
+  if (account.nps_score > 50) score += 10;
+  else if (account.nps_score < 30) score -= 15;
+  return Math.max(0, Math.min(100, score));
 }
